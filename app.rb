57ea7bd98c96d11c281705f52abde411c060af35
onyoo <roberto9m9@gmail.com>
2015-12-15 13:51:47 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "gksrnr"
+  end
+
+  get "/" do
+    @session = session
+    erb :index
+  end
+
+  post "/checkout" do
+    session[:item]=params["item"]
+    erb :checkout
+  end
 end
