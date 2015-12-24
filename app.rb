27763e83ca9bce584dfca29181ccc8498f52a5d6
plai217 <philiplai217@gmail.com>
2015-12-24 17:13:48 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "opensesame"
+  end
+
+  get '/' do 
+    @session = session
+    erb :index
+  end
+
+  post "/checkout" do
+    session[:item] = params["item"]
+    session[:test] = params["test"]
+    erb :checkout
+
+  end
 end
+
