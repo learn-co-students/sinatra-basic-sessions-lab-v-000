aab2529407236b516787f2fb810c36bf2ced6d56
preetdha <manpreet.dha@gmail.com>
2016-01-30 19:55:20 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,34 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do 
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+  session["item"] = params[:item]
+  @session = session
+  erb :checkout
+  end
 end
+
+# class Item
+#   attr_accessor :item
+#   ITEMS = []
+
+#   def initialize(params)
+#     @item = params[:item]
+#     ITEMS << self    
+#   end
+
+#   def self.all
+#     ITEMS
+#   end
+
+# end
