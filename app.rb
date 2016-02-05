4313d54578f32972f25d6b832d1e2bf9a7f6206d
simque <simon.duncanson@gmail.com>
2016-02-05 08:30:55 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+configure do
+  enable :sessions
+  set :session_secret, "secret"
 end
+
+class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    # session.merge!(params)
+    session[:item] = params.values.first
+    @session = session
+    erb :checkout
+  end
+end
+
+# the merge method did not work. i had to manually add the key, :item
+# to the session hash and set it equal to a value.  something about this
+# seems incorrect. i will compare with the solution.
