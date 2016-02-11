7b4b03613306922da63ac8bcf05b11106221c3e4
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-02-11 19:43:34 +0000

    completed, changed the spec_helper.rb so capybara would run.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post "/checkout" do 
+    @item_added = params[:item]
+    session[:item] = @item_added
+    @sessions = session
+    erb :checkout
+  end
 end
