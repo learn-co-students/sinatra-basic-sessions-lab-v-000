0be31bbfc93abe590afdcedc48b749dce0924643
adriandave13 <adrian@paradoxlogic.net>
2016-05-02 13:54:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do 
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session[:item] = params[:item]
+    puts "Session"
+    @session.each do |key, value|
+      puts "#{key}: #{value}"
+    end
+    erb :checkout
+  end
+
 end
