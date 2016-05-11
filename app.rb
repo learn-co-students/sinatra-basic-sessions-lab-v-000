dfb59e0bb84613f934c7981eea246d197829ef44
mcglaser <michaelcharlesglaser@gmail.com>
2016-05-11 23:56:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+  enable :sessions
+  set :session_secret, "mystery"
+end
+
+  get '/' do
+  @session = session
+
+  erb :index
+end
+
+  post '/checkout' do
+  session["item"] = params[:item]
+
+  erb :checkout
+end
+
+  get '/checkout' do
+    @session = session
+
+   erb :checkout
+  end
+
+
 end
