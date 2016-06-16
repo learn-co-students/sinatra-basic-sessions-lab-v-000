368a98951907cbbce98189925ecd854b93249c81
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-06-16 22:07:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+      @get_p = params[:item]
+      @session = session
+      @session[:item] = @get_p
+  end
+
 end
