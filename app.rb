5d933f5ab353c894e672163b5e313c0e7f9fe1f6
wedesignstudios <deelden@gmail.com>
2016-06-24 11:02:54 -0500

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "642tinythings"
+  end
+
+  get '/' do    
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    @params = params
+    erb :item
+  end
+
 end
