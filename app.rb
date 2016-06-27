1f8aa3b0643d003c6f9b982a21974a317475b3e5
jwolfe890 <jwolfe890@gmail.com>
2016-06-27 18:49:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do 
+  erb :index
+end
+
+post '/checkout' do
+  session[:item] = params[:item]
+  # binding.pry 
+  @session = session
+
+end 
+
+  # session["name"] = "Victoria"
+  # @session = session
+
 end
