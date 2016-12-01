b506de2dbc84e289071da266d46ce7794b261157
Jon Fougner <jon.fougner@gmail.com>
2016-12-01 18:48:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
+require 'pry'
+
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    # binding.pry
+    session["item"] = params[:item]
+    @session = session
+    # binding.pry
+  end
+
 end
