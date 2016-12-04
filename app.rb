964f518c143268259cd0d691aca40c87ec5568da
amcooper <amcooper@gmail.com>
2016-12-04 18:48:56 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do 
+    enable :sessions
+    set :session_secret, "sphagnum-esplanade"
+  end
+
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @view_session = session
+    @view_session[:item]
+  end
 end
