b35c6ff6fad50120b5bf6ff2cd4065fd063d5d99
Senad Hadrovic <github email address>
2017-01-07 23:18:34 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do 
+		erb :index 
+	end 
+
+	post '/checkout' do
+	 session[:item] = params[:item]
+	 @session = session
+	  binding.pry
+	 erb :show
+	 binding.pry
+	end 
 end
