7555064af48109d99a49d3da2095ff39dc02ebba
Scott Young <young.scottw@gmail.com>
2017-01-25 15:58:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    puts params
+    @session = session
+    @session.merge!(item: params[:item])
+    erb :checkout
+  end
 end
