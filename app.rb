64972d299949c33b06f94660339b917e29c77971
vanita eckert <vanita112003@gmail.com>
2017-02-15 19:42:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+
+  post "/checkout" do
+  @session = session
+@new_hash = @session.merge!(params)
+    @item = @new_hash["item"]
+  erb :display
+  end
+
+
+
+  
 end
