1d6ebbd5ace61d65371504359d6c2f6189d421f7
Annette  Drapalski <annette.drapalski@gmail.com>
2017-04-12 04:09:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
-require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "12345"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  post "/checkout" do
+    @session = session
+    puts params[:item]
+    @session[:item] = params[:item]
+    erb :checkout
+  end
 end
