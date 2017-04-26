6dd69b8f7f70d4ee8442557d8ba5433673935887
Alex Pepper <axpepper@gmail.com>
2017-04-25 20:21:11 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :sessions_secret, "secret"
+  end
+
+  get '/' do
+    @session = session
+    erb :index
+  end
+  
+  post '/checkout' do
+    @session = session
+    @session['item'] = params[:item]
+    "<h1>Cart Results:</h1>
+    <p>#{params[:item]}</p>"
+  end
 end
