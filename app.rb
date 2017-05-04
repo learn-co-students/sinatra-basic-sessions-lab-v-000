3833cd898093f3b032c90b26dc1eb8da0ca91d0a
Parv Kumar <pkumar@trinity.edu>
2017-05-04 18:44:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do 
+    erb :index
+  end 
+  post '/checkout' do 
+    @item = params[:item]
+    session['item'] = @item
+    "You have selected #{@item}."
+  end 
+
 end
