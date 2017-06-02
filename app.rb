8b3bed9fef7e28607fd2e136e1ac044228967518
Saad <Dropheaven@live.com>
2017-06-01 23:02:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -13,7 +13,8 @@ class App < Sinatra::Base
 
   post "/checkout" do
     @session = session
-    @session[:item] = params["item"]
+    @session[:item] = params[:item]
+    erb :checkout
   end
 
 end
