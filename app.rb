99fd1166b5320b5791748cc5c2a63db0b99852e5
streamedLine <dovidlwtn@gmail.com>
2017-06-09 16:03:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	configure do 
+		enable :sessions
+		set :session_secret, 'secret'
+	end
+
+	get '/' do 
+		erb :index
+	end
+
+	get '/confirmation' do 
+		@item = session[:item]
+		session[:item] = nil
+		
+		erb :confirmation
+	end
+
+	post '/checkout' do 
+		session[:item] = params[:item]
+
+		redirect to :'/confirmation'
+	end
+
 end
