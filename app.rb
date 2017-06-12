c35ac85d9c2495c4ef53cf05472ab13e17fa7056
Dave Schlauderaff <dschlaud@gmail.com>
2017-06-12 15:58:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  set :views, Proc.new { File.join(root, "/views/") }
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    # binding.pry
+    session[:item] = params[:item]
+    @session = session
+    erb :cart
+  end
+
+
 end
