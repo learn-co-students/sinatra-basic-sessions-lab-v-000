65d6656afa0c8adf1e0b098c98738db635cf1689
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-06-29 18:00:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :Sessions
+    set :session_secret, "encryptz0r"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  post "/checkout" do
+    session[:item] = params[:item]
+    @session = session
+    erb :item
+  end
 end
