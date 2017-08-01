b9a8667602885929b720a102ba30fcb3fff71e33
Leonardo Guzman <lguzman@me.com>
2017-08-01 04:33:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	configure do
+		enable :sessions
+		set :session_secret, "secret"
+	end
+
+	get '/' do
+		@session = session
+		erb :index
+	end
+
+	post '/checkout' do
+		#The key should be item and the value should be the item the user entered to the purchase.
+		session[:item] = params[:item]
+		# store the session hash in an instance variable that you can access in the views.
+		@session = session
+		erb :checkout
+	end
+
 end
