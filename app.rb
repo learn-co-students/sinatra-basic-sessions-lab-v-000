d8391d6d35616a0f20a25096bedbacc55e9cb414
Cynthia Rittenbach <crittenbach@gmail.com>
2017-09-05 00:59:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+ get '/' do
+   erb :index
+ end
+
+ post '/checkout' do
+   @item = params
+   session[:item] = @item["item"]
+
+   erb :checkout
+
+ end
+
+
+
 end
