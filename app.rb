60bdad565828476fbec258f85c889c34f5892ff1
Donna <donnamcla@gmail.com>
2017-09-26 23:23:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :sessions_secret, "cacao"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        @session = session
+        session[:item] = params[:item]
+        @session
+    end
+
 end
