689c3ad0d6b8912338bacccc6e5fa3d0360f5687
Jennifer Nelson <jennifernelson118@gmail.com>
2017-10-09 17:49:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = params.each do |k,v|
+      session[k] = v
+    end
+    erb :checkout
+  end
+
 end
