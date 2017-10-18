76e7afc019bec02ea728f04981c8e4aa5f74467d
Tavish Harrison <creationmargin26@gmail.com>
2017-10-18 01:06:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :'index'
+  end
+
+  configure do
+    enable :sessions
+    set :sessions_secret, "hogwarts"
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
 end
