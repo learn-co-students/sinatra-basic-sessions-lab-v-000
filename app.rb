f6e7bf0ebb42ca282b1048716ff1f8ac33232c52
ryanwevans <ryanwevans@icloud.com>
2017-10-18 21:37:49 -0700

    added routes, session enabled, form for cart and view items in cart

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/hey' do
+    @session = session
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    erb :checkout
+  end
 end
