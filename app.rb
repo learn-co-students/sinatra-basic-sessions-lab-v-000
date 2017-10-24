f3121514eb2d674cae723e7e1783ad1969a08730
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-24 17:37:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  # Configure sessions hash, set session secret
+  configure do
+    enable :sessions
+    set :session_secret, "pootie_tang"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  # Set item key to session[:item] from params[:item] from name='item'
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :index
+  end
+
 end
