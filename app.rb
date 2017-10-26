7066151e6cf7063b2dcf8b96d965c9816f54473c
GusVilla303 <gusvilla303@gmail.com>
2017-10-26 13:22:30 -0600

    Add '/checkout' route, post action, and set item in session cookie

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "my_secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+  end
 end
