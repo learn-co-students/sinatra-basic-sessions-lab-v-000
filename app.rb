2e30d4dacda8f106cc747e83cdc5f04f1e8ae9ce
alpbourne <alpbourne@gmail.com>
2017-11-13 16:24:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :Sessions
+    set :session_secret, "learnco"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+
 end
