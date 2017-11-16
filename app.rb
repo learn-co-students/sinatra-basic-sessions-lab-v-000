bb10bcb2545ebf013b8c1dcd487aeaddd60e3a47
Vicky Lau <vlau.nyc@gmail.com>
2017-11-16 21:00:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, 'encription_key_that_you_wont_guess'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @shopping_cart = session[:item]
+    @session = session
+
+    erb :checkout
+  end
+
 end
