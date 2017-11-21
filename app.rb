43508533e300dd644f8b96f3d39f9f7d4ae02731
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-21 01:22:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  # 1.enable
+  # 2. set secrete
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  
+
+
 end
