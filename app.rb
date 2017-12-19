004ac72d4b6bc977e489705710cd047b63142371
dbfarms <matt.hunger@gmail.com>
2017-12-19 15:37:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do 
+    enable :sessions 
+    set :session_secret, "secret"
+  end 
+  
+  get '/' do 
+    
+    erb :index
+  end 
+  
+  post '/checkout' do 
+    
+    #binding.pry 
+    @params = params 
+    
+    session["item"] = @params["item"] 
+    
+    erb :view
+    
+  end 
+  
 end
