1c04b5e95fc20ec083ee4b077b483727feb14b60
Caroline Shields <caroline.shields@amkor.com>
2018-01-12 00:08:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  configure do
+    enable :sessions
+    set :session_secret, "weeeee"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :items
+  end
+
+end #ends App
