ecbf7df17893d07635ed210f81a75ab6ec7d4b02
github username <github email address>
2018-01-13 22:19:44 -0600

    Done.

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ class App < Sinatra::Base
     end
 
     post '/checkout' do
-        session[:item] = params[:item]
+        # session[:item] = params[:item]
+        session["item"] = "socks"
         @session = session
         erb :checkout
     end
