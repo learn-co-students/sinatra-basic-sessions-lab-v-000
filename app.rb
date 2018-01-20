1005816ff34f51bd150b93ea21ae40ce71f863b0
ronald vilorio <soccer23415@gmail.com>
2018-01-20 22:01:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  get '/' do
+    erb :index
+  end
+  post '/checkout' do
+    binding.pry
+    @session = session
+    erb
+  end
 end
