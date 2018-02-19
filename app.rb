ef368f188e0f0278087a02be9c5ce2c9062ea6a1
github username <github email address>
2018-02-19 11:49:32 -0600

    Done.

--- app.rb
+++ app.rb
@@ -7,13 +7,12 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    @session = session
     erb :index
   end
 
   post '/checkout' do
     session[:item] = params[:item]
-    @item = session[:item]
+    @session = session
     erb :checkout
   end
 end
