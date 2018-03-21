25759efb180a39ef1048158960a97cea0d344c90
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-21 17:54:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
+
+
 class App < Sinatra::Base
+   
+   configure do 
+     enable :sessions 
+     set :sessions_secret, "goober"
+   end 
+   
+   get '/' do 
+     erb :index 
+   end 
+   
+   post '/checkout' do 
+     session[:item] = params[:item]
+     @sessions = session
+     erb :checkout
+   end 
+  
 end
