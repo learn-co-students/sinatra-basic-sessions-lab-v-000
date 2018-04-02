9d878d71048cd9b5ea4a85040cdd8f584331f489
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-02 13:42:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+
+    set :sessions_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @cart = params
+
+    session["item"] = @cart[:item]
+    @session = session
+
+    erb :checkout
+  end
 end
