fdd9d830a236848a0f29c0982f50f5e2a3c8001d
John O'Connor <oconnojb@gmail.com>
2018-04-14 18:07:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session[:item] = params[:item]
+    "#{@session[:item]}"
+  end
 end
+
+# The controller action /checkout, should take the params from the form
+# and add it to the session hash. The key should be item
+# and the value should be the item the user entered to the purchase.
+# Make sure to store the session hash
+# in an instance variable that you can access in the views.
