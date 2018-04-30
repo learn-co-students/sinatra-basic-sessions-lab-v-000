78df6808d856c7710a2e9328cbf8844315c79bf4
Bryan Hooper <bryan@lindsayfarm.net>
2018-04-30 13:59:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, 'foolingyou'
+end
+
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    item = params['item']
+    session[:item] = item
+    @session = session
+
+    erb :checkout
+  end
+
+  
 end
