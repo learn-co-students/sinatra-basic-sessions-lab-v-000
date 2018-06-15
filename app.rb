a4e1f5676b3897fbd7ac5d5745a40444418a6176
Alex Wilson <fonzmp@gmail.com>
2018-06-15 02:19:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "tacosalad"
+  end
+
+  get '/' do
+    erb :"../views/index"
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :"../views/checkout"
+  end
+
 end
