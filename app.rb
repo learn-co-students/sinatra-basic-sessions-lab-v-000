9534727d385a3f0edf38ab8866a858ba322f1edc
Alex Wilson <fonzmp@gmail.com>
2018-06-15 03:01:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,8 +12,9 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-    session[:item] = params[:item]
     @session = session
+    session[:item] = params[:item]
+
     erb :"../views/checkout"
   end
 
