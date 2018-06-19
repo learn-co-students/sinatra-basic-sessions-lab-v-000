2c8245225892768e826bd8eac9ce870f11810125
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-19 15:40:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ class App < Sinatra::Base
   end
 
   get '/' do 
-    @session = session
     erb :index
   end
 
   post '/checkout' do
+    @session = session
     session[:item] = params[:item]
   end
   
