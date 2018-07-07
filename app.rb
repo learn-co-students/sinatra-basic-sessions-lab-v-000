77c7482e16813af93991646b278b9202447fe392
Pamela Frieze <pfrieze@gmail.com>
2018-07-07 12:38:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    session[:item] = params[:item]
+    erb :checkout
+  end
+
+  configure do
+    enable :sessions #turns sessions on
+    set :session_secret, "secret" #encryption key that creates a session id
+  end #stored in browser cookie
 end
