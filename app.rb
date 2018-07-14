d429feb91296acad37646acfb7a493f5ded07229
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-14 00:21:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "random"
+  end
+
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout'
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+  
 end
