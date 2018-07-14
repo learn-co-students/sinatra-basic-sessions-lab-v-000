29e15b94fd92cde8335f53e7ad24578716916adf
Kayla Reopelle <kreopelle@gmail.com>
2018-07-14 20:25:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :Sessions
+    set :session_secret, "pickles"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @item = params[:item]
+    
+    session[:item] = @item
+    @session = session
+
+    erb :checkout
+  end
+
 end
