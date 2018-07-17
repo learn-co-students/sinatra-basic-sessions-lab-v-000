d88e24374fb2cafdcbb1bdf414db735f8e627a57
Elaine Anderson <eparie@bu.edu>
2018-07-17 12:16:20 +0000

    session and form

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+get '/' do
+  erb :index
+end
+
+post '/checkout' do
+  @session = session
+@item = session["item"] = params[:item]
+
+erb :show
+end
+
+
 end
