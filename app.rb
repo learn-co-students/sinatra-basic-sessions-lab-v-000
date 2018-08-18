c6ea5c0857aeccef6e53b2af2b02e149a9ac94e2
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-18 23:09:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :sessions_secret, "secret"
+  end
+
+  get '/' do
+
+    erb :index
+  end
+
+  post '/checkout' do
+    @cart = params[:item]
+    session[:item] = params[:item]
+    
+    erb :view_cart
+  end
+
 end
