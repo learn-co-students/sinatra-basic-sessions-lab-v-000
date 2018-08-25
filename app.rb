d734feeef190ea7d91866fdb5974ca285d215e1a
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-24 22:35:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
+configure do
+   enable :sessions
+   set :session_secret, "hola mundo"
+end
+
 class App < Sinatra::Base
+
+   get '/' do
+      @session = session
+      erb :index
+   end
+
+   post '/checkout' do
+      session[:item] = params[:item]
+      @session = session
+   end
+
 end
