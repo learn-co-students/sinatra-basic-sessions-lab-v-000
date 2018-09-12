2132e5a1ac127fae63ca23bcf893498eda15ba9c
atxrenegade <harleighabel@gmail.com>
2018-09-12 15:31:18 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
+
 	configure do
 		enable :sessions
 		set :session_secret, "secret"
 	end
-
-	get "/" do
+	
+	get '/' do
 		erb :index
 	end
 
-	post "/checkout" do
-		session[:item] = params[:item]
+	post '/checkout' do
+		session["item"] = params[:item]
+		@session = session
+		erb :shopping_cart
 	end
 end
