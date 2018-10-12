6b64aefe646d238ae7ab361194628a1add8aeb32
Sushi Chanrai <sushi@pathworks.co>
2018-10-12 17:04:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
+require 'sinatra/base'
+
+
 
 class App < Sinatra::Base
+
+  configure do
+  enable :sessions
+  set :session_secret, "lakshmiji1"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session["item"] = params[:item]
+    @session = session
+  end
+
+
+
 end
