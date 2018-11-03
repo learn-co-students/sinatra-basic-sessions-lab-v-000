5cf704b669d0f87b17aba38dbfa34c756b77278e
Edsel Caprice <caprice@mail.usf.edu>
2018-11-03 15:39:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @cart = params["item"]
+    session["item"] = @cart
+
+    erb :shopping_cart
+  end
+
 end
