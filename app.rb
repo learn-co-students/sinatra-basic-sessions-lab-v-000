5c427fd9f2500b68bf79837d9aee660b76a38001
Samantha brake <sammy.brake@gmail.com>
2018-12-09 21:04:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+configure do
+  enable :sessions
+  set :session_secret, "111"
+end
+
+get '/' do
+  @session = session
+  erb :index
+end
+
+post '/checkout' do
+  @item = params[:item]
+  session['item'] = @item
+  erb :purchase
+  # binding.pry
+end
+
 end
