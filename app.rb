be1718332cd9031478ff02583ef56962f9c84b98
Connie Hyman <constance.hyman@mchsi.com>
2018-12-20 09:19:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,34 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+
+  get '/' do
+
+    erb :index
+  end
+
+  post '/checkout' do
+    # binding.pry
+
+    @item = Item.new(params[:item])
+
+    @items = Item.all
+
+    session["item"] = "socks"
+    @session = session
+
+    erb :checkout
+  end
+
 end
