115f166f0a76c3bbaa97f98a9f78bce30e8d2bb2
Sharmaine Requerme <srqrme@gmail.com>
2019-01-09 02:15:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   configure do
     enable :sessions
     set :session_secret, "vYUDPy2x"
+  end
+
+  get '/' do
+    erb :index
   end
 end
