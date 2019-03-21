04821b3e6bf04a0bee3f79873fadc964ed482e60
Cheryl K <cherylkelson@icloud.com>
2019-03-21 13:36:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "tiddlywinks"
+end
+
 class App < Sinatra::Base
+end
+
+
+  get '/'
+    erb :index
+  end
+
+
+  post '/checkout' do
+    @session = session
+    @session = {
+      "session_id" =>
+
+# key = item
+# value = item user entered to the purchase
+# store session hash in instance variable that can be accessed in views
+#
+    }
+    erb :'index/checkout'
+  end
+
 end
