d52c56f5c37b29195f6d7a1fe39bc9e6e21d56a0
Laura Messa Travin <laura.travin@gmail.com>
2019-04-05 21:42:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+     configure do
+       enable :sessions   #habilita la variable session con los datos
+       set :session_secret, "secret"
+     end
+
+     get '/' do
+      erb  :index
+     end
+
+     post '/checkout' do
+       
+       session[:item]= params[:item]  #guardo el item en el hash session.
+       @session = session #guardo el hash en una instancia
+     end
 end
