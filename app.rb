4ac4d6be483832a1e68f7f694704ad1b030f47a3
Amee Covarrubias <ameecova@yahoo.com>
2019-06-11 02:01:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  post '/checkout' do
+    session[:item] = params[:item]
+    
+    erb :checkout
+  end 
+  
+  configure do
+    enable :sessions
+    set :session_secret, "vixenisthebest"
+  end
 end
