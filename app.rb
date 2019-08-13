749c341c274d2255ef850483484a0a55d3120dd5
Diapause <diapause@protonmail.com>
2019-08-13 15:12:12 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, 'supercalafragalisticexpialidochius'
+
+  end
+
+  get '/' do
+    # @session = session
+    erb :index
+  end
+
+  post '/checkout' do
+    @item = params[:item]
+    # binding.pry
+    session[:item] = params[:item]
+  end
+
 end
