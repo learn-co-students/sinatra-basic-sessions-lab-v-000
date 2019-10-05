ae2ddecf2b668e3239a572e9e011b1e70a2ad9f1
Toni-Ty <antonique2479@yahoo.com>
2019-10-05 05:17:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable: sessions
+    set :sessions_secret, "secret"
+
+
+
+get '/' do
+  erb  :index
+end
+
+post '/checkout' do
+
+sessions
+
+  erb :
+end
+
+
+
 end
