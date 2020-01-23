5554db609aa82a4cf92d8f3a874bf97126ffe14e
Jonathan Fernandez <jfern20@gmail.com>
2020-01-23 03:16:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,13 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do 
-  erb  :index
-  end 
- 
-  post '/checkout' do
-    "socks i bought!"
+
+  configure do
+    enable :sessions
+    set :session_secret, 'flatiron'
   end
-  
-  
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do 
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+
 end
