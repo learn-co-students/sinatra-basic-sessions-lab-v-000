eaa09d0590f20fac27c4e6d4f165be9140487b8d
Lamia Haider <lamia.haider@gmail.com>
2021-02-01 19:02:49 -0500

    Done.

---
+++ models/item.rb
@@ -0,0 +1,16 @@
+# class Item
+
+#     attr_accessor :name
+
+#     @@all = []
+
+#     def initialize(params)
+#         @name = name
+#         @@all << self
+#     end
+
+#     def self.all
+#         @@all
+#     end
+
+# end
